608473771eb1d22849af5cd62740b52914f61dfe
Benson Leung <67038212+leungbenson@users.noreply.github.com>
2022-02-27 20:00:53 +0000

    forked flask

--- flask/app.py
+++ flask/app.py
@@ -1,18 +1,19 @@
-from flask import Flask # Import Flask Class
+from flask import Flask
 from flask import render_template
 
-import random 
+import random
 
-app = Flask(__name__) # Create an Instance
+app = Flask(__name__)
 
-#Example of a route that passes
-#data to a template
+
+# Example of a route that passes
+# data to a template
 @app.route("/rand")
-def randonnumber():
+def randomnumber():
   i = random.randrange(100)
-  return render_template("lucky.html",number = i)
+  return render_template("lucky.html",number = i )
 
-#the "root" route
+# the "root" route
 @app.route("/")
 def index():
   return "<h1>Hello World from Repl.it!</h1>"
@@ -22,14 +23,13 @@ def index():
 def about():
   return render_template("about.html")
 
-#return 
-#example of static content
-#like an image of includiong Class
-@app.route("image_css")
-  def image_css():
-    return render_template("image_css.html")
-
-app.run(host='0.0.0.0', port=5000, debug=True) # Run the Application (in debug mode)
+# example of static content
+# like an image or including css
+@app.route("/image_css")
+def image_css():
+  return render_template("image_css.html")
+  
+app.run(host="0.0.0.0",port=5000,debug=True) # Run the Application (in debug mode)
 
   
 #@app.route('/') # Route the Function
